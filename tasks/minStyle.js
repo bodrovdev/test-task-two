87340fa81f50f360ef11a8fe379cb7e358a9f992
Igor Bodrov <bodrovigor02@gmail.com>
2022-07-14 22:11:34 +0300

    feat: 🎸 Добавил разметку

---
+++ tasks/minStyle.js
@@ -0,0 +1,29 @@
+const {src, dest}      = require('gulp');
+const autoprefixer     = require('autoprefixer');
+const clean 					 = require('gulp-clean-css');
+const concat 					 = require('gulp-concat');
+const plumber 				 = require('gulp-plumber');
+const postcss 				 = require('gulp-postcss');
+const postcssColorMod  = require('@alexlafroscia/postcss-color-mod-function');
+const postcssPresetEnv = require('postcss-preset-env');
+const scss  				   = require('gulp-sass')(require('sass'));
+
+// Минификация CSS
+module.exports = function minStyle(browserSync) {
+	const plugins = [
+		postcssPresetEnv(),
+		postcssColorMod({
+			unresolved: 'warn',
+		}),
+		autoprefixer(),
+	];
+
+	return src('src/scss/**/*.scss')
+		.pipe(plumber())
+		.pipe(scss({outputStyle: 'compressed'}))
+		.pipe(postcss(plugins))
+		.pipe(clean({level: 2}))
+		.pipe(concat('style.min.css'))
+		.pipe(dest('build/css/'))
+		.pipe(browserSync.stream())
+};
